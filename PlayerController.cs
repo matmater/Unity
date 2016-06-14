476c4d3f0b1570269ac69cee68a142e5bad7c134
matmater <santerim@gmail.com>
2016-06-14 23:52:35 +0300

    Ship rotation and height

---
+++ PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float turn;
+	public float ascDesc;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		//define in input manager !!
+		float turnShip = Input.GetAxis ("Turning");
+		float heightAdjustment = Input.GetAxis ("Height");
+
+		Vector3 movement = new Vector3 (moveHorizontal, ascDesc * heightAdjustment, moveVertical);
+
+		rb.AddRelativeForce (movement * speed);
+		rb.AddRelativeTorque (Vector3.up * turnShip * turn);
+	
+	}
+}

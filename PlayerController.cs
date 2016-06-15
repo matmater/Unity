d7b90de459c5df48fb2be1e7e81e5c4107a1ad1b
matmater <santerim@gmail.com>
2016-06-16 00:10:10 +0300

    Raycasting

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float turn;
-	public float ascDesc;
+	public float AscDesc;
 
 	private Rigidbody rb;
 
@@ -18,14 +18,22 @@ public class PlayerController : MonoBehaviour {
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		//define "Turning" and "Height" in input manager !!
+		//define in input manager !!
 		float turnShip = Input.GetAxis ("Turning");
 		float heightAdjustment = Input.GetAxis ("Height");
 
-		Vector3 movement = new Vector3 (moveHorizontal, ascDesc * heightAdjustment, moveVertical);
-
+		Vector3 movement = new Vector3 (moveHorizontal, AscDesc * heightAdjustment, moveVertical);
 		rb.AddRelativeForce (movement * speed);
 		rb.AddRelativeTorque (Vector3.up * turnShip * turn);
+
+		if (Input.GetKeyDown ("space")) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width*0.5f, Screen.height*0.5f, 0.0f));
+			Physics.Raycast (ray, out hit, 500);
+			Debug.DrawLine(ray.origin, hit.point, Color.green, 2, false);
+			print ("pew!");
+			//hit.transform.SendMessage ("Hit");
+		}
 	
 	}
 }
